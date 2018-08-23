8b66cd065ba7d7189079dff2f74ede414d698037
M <M@a.com>
2018-08-23 19:51:44 +0100

    login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,17 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/login", name="login")
      */
-    public function index()
+    public function login(AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('security/admin.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 }
